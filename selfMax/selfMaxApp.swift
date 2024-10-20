47c3c24c571f16ae02935c560b398f4f057bed41
LiamiDev <liamsschoolwork314@gmail.com>
2024-10-20 15:29:31 +0300

    Initial Commit

---
+++ selfMax/selfMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  selfMaxApp.swift
+//  selfMax
+//
+//  Created by Liam Timberg on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct selfMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
